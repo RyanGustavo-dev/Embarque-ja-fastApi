bfda9f8557b200cc6a78272ba17312e9d0d0be39
Ryan Gustavo Torres <ryryan009@gmail.com>
2026-03-04 19:26:19 +0000

    feat: add configs

--- embarqueja-fast/embarqueja_fast/app.py
+++ embarqueja-fast/embarqueja_fast/app.py
@@ -1,63 +1,122 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from embarqueja_fast.schema import UserDB, UserList, UserPublic, UserSchema
+from embarqueja_fast.database import get_session
+from embarqueja_fast.Models import User
+from embarqueja_fast.schema import UserList, UserPublic, UserSchema
+from embarqueja_fast.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
-database = []
 
 
 @app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session=Depends(get_session)):
 
-    database.append(user_with_id)
-    return user_with_id
+    dbUser = session.scalar(
+        select(User).where(
+            (User.name == user.name) | (User.email == user.email)
+        )
+    )
+
+    if dbUser:
+        if dbUser.name == user.name:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exist',
+            )
+        elif dbUser.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exist',
+            )
+
+    newDb = User(
+        name=user.name,
+        email=user.email,
+        password=get_password_hash(user.password),
+    )
+
+    session.add(newDb)
+    session.commit()
+    session.refresh(newDb)
+
+    return newDb
 
 
 @app.get('/users', status_code=HTTPStatus.OK, response_model=UserList)
-def get_all():
-    return {'users': database}
+def get_all(
+    limit: int = 10, skip: int = 0, session: Session = Depends(get_session)
+):
+
+    users = session.scalars(select(User).limit(limit).offset(skip))
+    return {'users': users}
 
 
 @app.put(
     '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
 )
-def update_user(user: UserSchema, user_id: int):
-
-    if user_id < 1 or user_id > len(database):
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-        )
-
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
-
-    return user_with_id
-
-
 @app.delete(
-    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+    '/users/{user_id}',
+    status_code=HTTPStatus.OK,
+    response_model=UserPublic,
+    current_user=Depends(get_current_user),
 )
-def delete_user(user_id: int):
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    dbUser = session.scalar(select(User).where(User.id == user_id))
 
-    if user_id < 1 or user_id > len(database):
+    if not dbUser:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
-    return database.pop(user_id - 1)
+    session.delete(dbUser)
+    session.commit()
+
+    return {'message': 'User deleted'}
 
 
-@app.get(
-    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
-)
-def get_user_by_id(user_id: int):
-
-    if user_id < 1 or user_id > len(database):
+@app.post('/tolken')
+def login_for_acess_tolken(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+    if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=400, detail='Incorrect email or password'
         )
 
-    return database[user_id - 1]
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'Bearer'}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
+        )
+
+    current_user.username = user.name
+    current_user.password = get_password_hash(user.password)
+    current_user.email = user.email
+
+    session.commit()
+    session.refresh(current_user)
+
+    return current_user
--- embarqueja-fast/tests/test_app.py
+++ embarqueja-fast/tests/test_app.py
@@ -1,8 +1,6 @@
 from http import HTTPStatus
 
-from sqlalchemy import select
-
-from embarqueja_fast.Models import User
+from embarqueja_fast.schema import UserPublic
 
 
 def test_criar_cliente(client):
@@ -24,25 +22,18 @@ def test_criar_cliente(client):
     }
 
 
-def test_buscar_clientes(client):
-
+def test_buscar_clientes_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'name': 'alice',
-                'email': 'alice@example.com',
-            },
-        ]
-    }
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_atualizar_cliente(client):
+def test_atualizar_cliente(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'name': 'alice2',
             'email': 'alice2@example.com',
@@ -57,66 +48,3 @@ def test_atualizar_cliente(client):
         'name': 'alice2',
         'email': 'alice2@example.com',
     }
-
-
-def test_atualizar_cliente_falho(client):
-    response = client.put(
-        '/users/0',
-        json={
-            'name': 'alice2',
-            'email': 'alice2@example.com',
-            'password': 'secret',
-        },
-    )
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_buscar_cliente_pelo_id(client):
-    response = client.get('/users/1')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'name': 'alice2',
-        'email': 'alice2@example.com',
-    }
-
-
-def test_buscar_cliente_pelo_id_falho(client):
-    response = client.get('/users/0')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_deletar_cliente(client):
-    response = client.delete('/users/1')
-
-    assert response.status_code == HTTPStatus.OK
-
-    assert response.json() == {
-        'id': 1,
-        'name': 'alice2',
-        'email': 'alice2@example.com',
-    }
-
-
-def test_deletar_cliente_falho(client):
-    response = client.delete('/users/0')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_create_new_user(session):
-
-    user = User(email='test@email.com', name='testName', password='pass')
-
-    session.add(user)
-    session.commit()
-
-    response = session.scalar(select(User).filter(User.email == user.email))
-
-    assert response.name == 'testName'
